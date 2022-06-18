b06a4702db044adc67752ec6288571732fd0fc59
Vertilix <vertilix0@gmail.com>
2022-06-18 22:49:26 +0200

    Helemaal opnieuw gemaakt, Je kan nu een pokemon kiezen en het reset niet als je de pagina herlaad of aanvalt. Code is ook verbeterd.

--- index.php
+++ index.php
@@ -1,116 +1,75 @@
 <?php
 session_start();
 error_reporting(-1);
-ini_set( 'display_errors', 1 ); // Op arch linux liet hij geen errors zien maar met deze functies wel, Misschien is er een andere oplossing maar voor nu niet nodig
-
-if(isset($_GET['reset'])){ // Reset knop
-    session_destroy();
-    header('location:index.php');
-}
-
-$selectedPokemon = new Pokemon;
-$enemyPokemon = new Pokemon;
-
-if(isset($_SESSION['hp'])) {
-    $selectedPokemon->hp = $_SESSION['hp'];
-    $enemyPokemon->hp = $_SESSION['eHp'];
-}else {
-    $enemyPokemon->setHp(100);
-    $selectedPokemon->setHp(100);
-}
-
-if(isset($_GET["pokemon"])) {
-    $pokemon = $_GET["pokemon"];
-    $ePokemon = $_GET["ePokemon"];
-}else {
-    $pokemon = "";
-    $ePokemon = "";
-}
-
-if(isset($_GET['attack'])){
-    damage();
-}
-
-$shown = "none";
-
+ini_set( 'display_errors', 1 );
 class Pokemon {
     public $name;
     public $type;
     public $hp;
-
-    function setName($name){
-        $this->name = $name;
-    }
-
-    function setHp($hp) {
-        $this->hp = $hp;
-    }
-
-    function setType($type){
-        $this->type = $type;
-    }
 }
-
 $names = array("Charmander", "Bulbasaur", "Squirtle");
 $types = array("Fire", "Grass", "Water");
 
-function createPokemon($n, $t) {
-    global $names;
-    global $types;
-    global $selectedPokemon;
-    $selectedPokemon->setName($names[$n]);
-    $selectedPokemon->setType($types[$t]);
-    echo 'Jij koos "' . $selectedPokemon->name. '"!<br>';
-    echo 'Met type "' . $selectedPokemon->type. '"!<br>';
+$jPokemon = new Pokemon();
+
+$jPokemon->hp = 100;
+
+$ePokemon = new Pokemon();
+$ePokemon->name = $names[1];
+$ePokemon->type = $types[1];
+$ePokemon->hp = 100;
+
+
+if(isset($_SESSION['jPokemon'])) {
+    $jPokemon = $_SESSION['jPokemon'];
+    $ePokemon = $_SESSION['ePokemon'];
+}else{
+    $jPokemon->hp = 100;
+    $ePokemon->hp = 100;
 }
 
-function createBattle() {
-    global $enemyPokemon;
-    global $names;
-    global $types;
-    global $ePokemon;
-    $enemyPokemon->name = $ePokemon;
-
-    if ($enemyPokemon->name == "Charmander") {
-        $enemyPokemon->setType($types[0]);
-        $enemyPokemon->setName($names[0]);
-    }elseif ($enemyPokemon->name  == "Bulbasaur") {
-        $enemyPokemon->setType($types[1]);
-        $enemyPokemon->setName($names[1]);
-    }else{
-        $enemyPokemon->setType($types[2]);
-        $enemyPokemon->setName($names[2]);
-    }
-    echo $enemyPokemon->type;
-    echo $enemyPokemon->name;
-    echo $enemyPokemon->hp;
+if(isset($_GET['attack'])){
+    damage($jPokemon, $ePokemon);
 }
 
-$_SESSION["hp"] = $selectedPokemon->hp;
-$_SESSION["eHp"] = $enemyPokemon->hp;
-
-function damage(){
-    $defaultDmg = 20;
-    if
+if(isset($_GET['reset'])){ // Reset knop
+    session_destroy();
+    header('location:rewrite.php');
 }
 
-if ($pokemon == "Charmander") {
-    createPokemon(0, 0);
-    createBattle();
-}elseif ($pokemon == "Bulbasaur") {
-    createPokemon(1, 1);
-    createBattle();
-}elseif ($pokemon == "Squirtle"){
-    createPokemon(2, 2);
-    createBattle();
-}else {
-    $shown = "block";
+if(isset($_GET['pokemon'])){
+    $pokeName = $_GET['pokemon'];
+    $jPokemon->name = $pokeName;
 }
 
-echo $enemyPokemon->name;
-echo $selectedPokemon->name;
+function createPokemon($i,$n,$t){
+    $i = new Pokemon;
+    $i->name = $n;
+    $i->type = $t;
+}
 
+function damage($a, $d) {
+    $defaultDamage = 20;
+    $d->hp -= $defaultDamage;
+    echo $a->name ." did ". $defaultDamage . " damage to ". $d->name;
+}
+
+if ($ePokemon->hp <= 0) {
+    death($ePokemon);
+}elseif ($jPokemon->hp <= 0){
+    death($jPokemon);
+}
+
+function death($d){
+    echo $d->name." is dood!";
+    $d->hp = 0;
+}
+
+echo "<br>";
+echo $jPokemon->hp;
+echo $ePokemon->hp;
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -118,35 +77,27 @@ echo $selectedPokemon->name;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Pokemon</title>
-    <style>
-        .shown {
-            display: <?php echo $shown?>
-        }
-    </style>
 </head>
 <body>
-    <form action="" method="GET">
-        <div class="shown">
-            <p>Kies een pokémon</p>
-            <select name="pokemon">
-                <option>Charmander</option>
-                <option>Bulbasaur</option>
-                <option>Squirtle</option>
-            </select>
-            <select name="ePokemon">
-                <option>Charmander</option>
-                <option>Bulbasaur</option>
-                <option>Squirtle</option>
-            </select>
-            <button type="submit">Select</button>
-        </div>
-
-        <button type="attack">Attack</button>
+    <form method="GET" action="">
+        <select name="pokemon">
+            <option>Charmander</option>
+            <option>Bulbasaur</option>
+            <option>Squirtle</option>
+        </select>
+        <input type="submit" value="Kies">
     </form>
 
-    <div class="buttons">
+    <form method="GET">
+        <input type="submit" name="attack" value="Val aan">
+    </form>
     <a href="?reset">Reset</a>
-</div>
 </body>
 </html>
+
+<?php
+    $_SESSION["jPokemon"] = $jPokemon;
+    $_SESSION["ePokemon"] = $ePokemon;
+?>
